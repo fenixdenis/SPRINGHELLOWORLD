edd5fec6a3294a78e62ebeaa0cd4cbe91c80c8bf
fenixdenis <sahajmarg99@gmail.com>
2025-01-01 20:28:49 +0300

    СОЗДАНИЕ

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,18 +1,15 @@
 public class Cat {
-    private String name; // Имя кота
-    private int age;     // Возраст кота
+    private String name;
+    private int age;
 
-    // Конструктор без параметров
     public Cat() {
     }
 
-    // Конструктор с параметрами
     public Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Геттеры и сеттеры
     public String getName() {
         return name;
     }
@@ -29,7 +26,6 @@ public class Cat {
         this.age = age;
     }
 
-    // Переопределение метода toString() для удобного вывода
     @Override
     public String toString() {
         return "Cat{" +
